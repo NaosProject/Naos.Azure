8eafca4d1108b6a60212948fe98b262792066697
Lawson Caudill <lawson@getthinktank.com>
2022-08-03 17:30:32 -0400

    Added blob client structure.

---
+++ Naos.Azure.Protocol.Blob.Client.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Naos.Azure.Protocol.Blob.Client.Test")]
+[assembly: AssemblyDescription("Naos.Azure.Protocol.Blob.Client.Test")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Naos Project")]
+[assembly: AssemblyProduct("Naos.Azure.Protocol.Blob.Client.Test")]
+[assembly: AssemblyCopyright("Copyright © Naos Project 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0c93d8b6-8883-4602-b451-abbfc61d6c2d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
---
+++ Naos.Azure.Protocol.Blob.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Naos.Azure.Protocol.Blob.Client")]
+[assembly: AssemblyDescription("Naos.Azure.Protocol.Blob.Client")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Naos Project")]
+[assembly: AssemblyProduct("Naos.Azure.Protocol.Blob.Client")]
+[assembly: AssemblyCopyright("Copyright © Naos Project 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("14234b98-c4f1-4282-a7a1-6b4b8765739c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
